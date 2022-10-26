fc1570c0cebb9ce4e5e7e84e2c48ba6698ca6d8c
Andrew-55 <tap55rus@gmail.com>
2022-10-27 04:00:44 +0600

    andrew.effect-matrix-words horizontally

--- src/canvas.js
+++ src/canvas.js
@@ -1,12 +1,12 @@
 const C = document.querySelector("canvas"),
   $ = C.getContext("2d"),
-  W = C.width = innerWidth,
-  H = C.height = innerHeight;
+  W = C.width = innerWidth*0.99
+  H = C.height = innerHeight*1.65;
 
-const str = "web JavaScriot SCSS html webpack Git js css ",
-matrix = str.split('')
+const str = "JavaScript html CSS webpack } module ( ",
+matrix = ["JavaScript", "body", "html", "Redux", "CSS", "git", "webpack", "}", "module", "(", "h1", "React", "//",]
 
-let font = 11,
+let font = 14,
 col = W / font,
 arr = []
 
@@ -18,7 +18,7 @@ function draw() {
 
     $.fillRect(0, 0, W, H)
   
-    $.fillStyle = "rgba(0, 0, 255, 0.3)"
+    $.fillStyle = "rgba(0, 0, 255, 0.2)"
 
     $.font = font + "px system-ui"
   
@@ -26,16 +26,17 @@ function draw() {
     for (let i = 0; i < arr.length; i++) {
 
       let txt = matrix[Math.floor(Math.random() * matrix.length)]
-  
-      $.fillText(txt, i * font, arr[i] * font)
+    
+
+      $.fillText(txt, i * font*Math.random() * 15, arr[i] * font)
       
-      if (arr[i] * font > H && Math.random() > 0.975) arr[i] = 0
+      if (arr[i] * font*20 > H && Math.random() > 0.98) arr[i] = 0
       
 
       arr[i]++
       }
   }
 
-setInterval(draw, 123)
+setInterval(draw, 500)
 
 window.addEventListener('resize', () => location.reload())
