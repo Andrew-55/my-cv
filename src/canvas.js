895b325060f5a45b34321ccb245bc826ce5e7481
Andrew-55 <tap55rus@gmail.com>
2022-11-01 19:47:33 +0600

    Andrew.added adaptive,change height canvas

--- src/canvas.js
+++ src/canvas.js
@@ -1,42 +1,39 @@
-const C = document.querySelector("canvas"),
-  $ = C.getContext("2d"),
-  W = C.width = innerWidth*0.99
-  H = C.height = innerHeight*1.65;
+const body = document.querySelector('body');
+const wb = body.offsetWidth;
+const hb = body.offsetHeight;
+const C = document.querySelector('canvas'),
+  $ = C.getContext('2d'),
+  W = C.width = wb
+  H = C.height = hb
 
-const str = "JavaScript html CSS webpack } module ( ",
-matrix = ["JavaScript", "body", "html", "Redux", "CSS", "git", "webpack", "}", "module", "(", "h1", "React", "//",]
-
+const str = '10111 11 100 00 111 0000 111 000',
+  matrix = str.split('');
 let font = 14,
-col = W / font,
-arr = []
+  col = W / font,
+  arr = [];
 
-for(let i = 0; i < col; i++) arr[i] = 1
+for (let i = 0; i < col; i++) arr[i] = 1;
 
 function draw() {
+  $.fillStyle = 'rgba(255, 255, 255, .05)';
 
-    $.fillStyle = "rgba(255, 255, 255, .05)"
+  $.fillRect(0, 0, W, H);
 
-    $.fillRect(0, 0, W, H)
-  
-    $.fillStyle = "rgba(0, 0, 255, 0.2)"
+  $.fillStyle = 'rgba(0, 0, 255, 0.2)';
 
-    $.font = font + "px system-ui"
-  
+  $.font = font + 'px system-ui';
 
-    for (let i = 0; i < arr.length; i++) {
+  for (let i = 0; i < arr.length; i++) {
+    let txt = matrix[Math.floor(Math.random() * matrix.length)];
 
-      let txt = matrix[Math.floor(Math.random() * matrix.length)]
-    
+    $.fillText(txt, i * font, arr[i] * font);
 
-      $.fillText(txt, i * font*Math.random() * 15, arr[i] * font)
-      
-      if (arr[i] * font*20 > H && Math.random() > 0.98) arr[i] = 0
-      
+    if (arr[i] * font * 20 > H && Math.random() > 0.98) arr[i] = 0;
 
-      arr[i]++
-      }
+    arr[i]++;
   }
+}
 
-setInterval(draw, 500)
+setInterval(draw, 250);
 
-window.addEventListener('resize', () => location.reload())
+window.addEventListener('resize', () => location.reload());
